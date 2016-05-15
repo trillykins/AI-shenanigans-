ab781ccb93d28ed04077c2694693bde157df27b5
s113611 <s113611@student.dtu.dk>
2016-05-16 01:16:59 +0200

    DAMN AA MAsharpagent not work

--- bin/searchclient.bat
+++ bin/searchclient.bat
@@ -8,4 +8,4 @@ javac conflicts/*.java
 :javac heuristics/*.java
 :javac strategies/*.java
 javac utils/*.java
-java -jar server.jar -l levels/%1.lvl -c "java searchclient.Run" -g
+java -jar server.jar -l levels/%1.lvl -c "java searchclient.Run" -g -p
