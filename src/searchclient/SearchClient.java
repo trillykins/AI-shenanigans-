f73b3aaca40573a28b56fa0ccf67b0524f23a41f
s113611 <s113611@student.dtu.dk>
2016-05-17 12:45:12 +0200

    update

--- src/searchclient/SearchClient.java
+++ src/searchclient/SearchClient.java
@@ -38,7 +38,7 @@ public class SearchClient {
 		in = new BufferedReader(new InputStreamReader(System.in));
 //		in = new BufferedReader(new FileReader("//Users//sunmengwei//Documents//levels//MARageQuit.lvl"));
 //		in = new BufferedReader(new FileReader("//Users//sunmengwei//Documents//levels//MAschwenke.lvl"));
-//		in = new BufferedReader(new FileReader("C://Users//Tjoe//Documents//levels//MAtbsAppartment.lvl"));
+//		in = new BufferedReader(new FileReader("C://Users//Tjoe//Documents//levels//MADangerbot.lvl"));
 		world = World.getInstance();
 	}
 
