7e2e3887cf031458e70f08a78c2b1b253fbc11de
s113611 <s113611@student.dtu.dk>
2016-05-17 14:37:42 +0200

    updates

--- src/searchclient/Run.java
+++ src/searchclient/Run.java
@@ -226,17 +226,20 @@ public class Run {
 					break;
 				}
 				replanned = true;
-				/*TESTIN : adding noOp for all other agents but the two conflicting ones*/
+				/*TESTING : adding noOp for all other agents but the two conflicting ones*/
 //				for(Agent agent : world.getAgents().values()){
+//					/*if the Receiver is null, then - find the closest agent and e
+//					 * xclude him from theupdate*/
 //					if(agent != con.getSender() && agent != con.getReceiver()){
 //						Node previousNode = null;
-//						if(agent.getStepInPlan() > 1)
-//							previousNode = agent.getPlan().get(agent.getStepInPlan()-2);
+//						if(agent.getStepInPlan() > 0)
+//							previousNode = agent.getPlan().get(agent.getStepInPlan()-1);
 //						else
 //							previousNode = agent.initialState;
 //						
 //						List<Node> newPlanAgentNoOp = Conflict.updatePlan(agent);
 //						Node noOp = Conflict.createNoOpNode(agent, previousNode);
+//						newPlanAgentNoOp.remove(0);
 //						newPlanAgentNoOp.add(0, noOp);
 //						agent.setPlan(newPlanAgentNoOp);
 //						agent.setStepInPlan(0);
@@ -259,7 +262,7 @@ public class Run {
 			}
 			world.updateBeliefs();
 			world.write("World:\n" + world.toString());
-			System.out.println("World:\n" + world.toString());
+//			System.out.println("World:\n" + world.toString());
 			world.write("Global goal state found = " + world.isGlobalGoalState());
 		}
 	}
